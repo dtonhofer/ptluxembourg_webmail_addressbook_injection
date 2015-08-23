3998a4afaf45c69a2059e27ee65632e7db684f28
dtonhofer <ronerycoder@gluino.name>
2015-08-23 21:10:49 +0200

    New stuff

---
+++ eclipse_project_root/src/name/heavycarbon/webmailfeed/Actions.java
@@ -0,0 +1,35 @@
+package name.heavycarbon.webmailfeed;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/* 34567890123456789012345678901234567890123456789012345678901234567890123456789
+ *******************************************************************************
+ * Determine which actions to perform
+ * 
+ * All the actions is the "actionSet" will be executed by "main()"; commment
+ * out any action that should not be performed!
+ * 
+ * 2015.08.23 - Moved out of main()
+ ******************************************************************************/
+
+public class Actions {
+
+    public enum Action {
+        addSubsetCommittee, addSubsetCeinturesNoires, addSubsetEnfants, addSubsetAdultes, addSubsetTousLesMembres
+    }
+
+    public static SortedSet<Action> ACTION_SET = new TreeSet<Action>();
+        
+    static {
+    	ACTION_SET.add(Action.addSubsetCommittee);
+    	ACTION_SET.add(Action.addSubsetCeinturesNoires);
+    	ACTION_SET.add(Action.addSubsetEnfants);
+    	ACTION_SET.add(Action.addSubsetAdultes);
+    	ACTION_SET.add(Action.addSubsetTousLesMembres);
+    }
+    
+    private Actions() {
+    	// cannot be instantiated
+    }
+}
